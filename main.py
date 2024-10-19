db3c3faa8b8ea50a76c499fbe58c80cbccd14c16
Vinícius <92545891+vxncius-dev@users.noreply.github.com>
2024-10-19 12:40:34 -0400

    Update main.py
    Seeking to make the app faster and lighter, choose to exchange the Flet graphic library for tkinter, which has some disadvantages in terms of design but is actually much faster in terms of component rendering; With some adjustments I managed to keep the app's design similar to what it was in flet, but some adjustments still need to be made so that it is fully optimized and complete.

--- main.py
+++ main.py
@@ -3,127 +3,139 @@ from os import path, makedirs, getenv, rename, listdir, remove, rmdir
 from ctypes import windll
 from yt_dlp import YoutubeDL
 from re import match, compile
+from winotify import Notification
 from requests import exceptions
-import flet as ft
 from pathlib import Path
 from typing import Optional, Dict, Tuple
 from time import sleep
+import tkinter as tk
+from tkinter import *
+from ctypes import windll
+
+GWL_EXSTYLE = -20
+WS_EX_APPWINDOW = 0x00040000
+WS_EX_TOOLWINDOW = 0x00000080
+
+
+class PlaceholderEntry(tk.Entry):
+    def __init__(self, master=None, placeholder="", *args, **kwargs) -> None:
+        super().__init__(master, *args, **kwargs)
+        self.placeholder = placeholder
+        self.bind("<FocusIn>", self.on_focus_in)
+        self.bind("<FocusOut>", self.on_focus_out)
+        self.insert(0, self.placeholder)
+        
+    def on_focus_in(self, event) -> None:
+        if self.get() == self.placeholder:
+            self.delete(0, tk.END)
+
+    def on_focus_out(self, event) -> None:
+        if not self.get():
+            self.insert(0, self.placeholder)
 
 
 class SnapDL:
-    def __init__(self) -> None:
-        self.app_name: str = "SnapDL"
-        self.icon: Path = Path(__file__).parent / "src" / "assets" / "icon.png"
-        self.icon_window: Path = Path(__file__).parent / "src" / "assets" / "icon.ico"
-        self.link_download: str = ""
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.offset_x = 0
+        self.offset_y = 0
         self.downloading_type: bool = True
         self.downloading: bool = False
+        self.is_minimized = False      
+        self.main()
+        
+    def main(self):
         self.ffmpeg_path: Path = Path(__file__).parent / "src"  / "ffmpeg" / "bin" / "ffmpeg.exe"
         self.ffmprobe_path: Path = Path(__file__).parent / "src"  / "ffmpeg" / "bin" / "ffmprobe.exe"
-        self.default_video_icon: Path = Path(__file__).parent / "src" / "assets" / "video.png"
-        self.default_music_icon: Path = Path(__file__).parent / "src" / "assets" / "music.png"
-        self.conclude_icon: Path = Path(__file__).parent / "src" / "assets" / "check.png"
-        self.fail_icon: Path = Path(__file__).parent / "src" / "assets" / "error.png"
+        self.icon: Path = Path(__file__).parent / "src" / "assets" / "icon.png"
+        self.window_icon: Path = Path(__file__).parent / "src" / "assets" / "icon.ico"
+        self.w_size, self.h_size = 400, 60 
         self.screen_width, self.screen_height = self.get_screen_dimensions()
-        ft.app(target=self.main, assets_dir="./assets")
+        pos_x, pos_y = self.screen_width - self.w_size - 15, self.screen_height - self.h_size - 65
+        self.root = tk.Tk()
+        self.root.title("SnapDL")
+        self.root.iconbitmap(self.window_icon)
+        self.root.geometry(f"{self.w_size}x{self.h_size}+{pos_x}+{pos_y}")
+        self.root.overrideredirect(True)
+        self.root.config(bg='grey')
+        self.root.attributes("-transparentcolor", "grey")
+        self.video_img = PhotoImage(file=r"./src/assets/video.png")
+        self.music_img = PhotoImage(file=r"./src/assets/music.png")
+        self.close_img = PhotoImage(file=r"./src/assets/close.png")
+        self.canvas = Canvas(self.root, bg="grey", highlightthickness=0)
+        self.canvas.place(x=0, y=0, relwidth=1, relheight=1)
+        self.round_rectangle(0, 0, self.w_size - 2, self.h_size - 2, radius=19, fill="#111111", outline="#444444", width=1)
+        self.root.bind("<Button-1>", self.start_move)
+        self.root.bind("<B1-Motion>", self.move_window)
+        self.build()
+        self.root.after(10, lambda: self.set_appwindow())
+        self.root.protocol("WM_DELETE_WINDOW", self.toggle_minimize_restore)
+        self.root.mainloop()
 
+    def minimize(self):
+        self.root.withdraw()
+        self.is_minimized = True
+
+    def restore_window(self):
+        self.root.deiconify()
+        self.is_minimized = False
+        
+    def toggle_minimize_restore(self):
+        self.restore_window() if self.is_minimized else self.minimize()   
+
+    def set_appwindow(self):
+        hwnd = windll.user32.GetParent(self.root.winfo_id())
+        style = windll.user32.GetWindowLongW(hwnd, GWL_EXSTYLE)
+        style = style & ~WS_EX_TOOLWINDOW
+        style = style | WS_EX_APPWINDOW
+        windll.user32.SetWindowLongW(hwnd, GWL_EXSTYLE, style)
+        self.root.wm_withdraw()
+        self.root.after(10, self.root.wm_deiconify)
+        
+    def round_rectangle(self, x1, y1, x2, y2, radius=25, **kwargs) -> int:
+        points = [
+            x1 + radius, y1, x1 + radius, y1, x2 - radius, y1, x2 - radius, y1, x2, y1,
+            x2, y1 + radius, x2, y1 + radius, x2, y2 - radius, x2, y2 - radius, x2, y2,
+            x2 - radius, y2, x2 - radius, y2, x1 + radius, y2, x1 + radius, y2, x1, y2,
+            x1, y2 - radius, x1, y2 - radius, x1, y1 + radius, x1, y1 + radius, x1, y1
+        ]
+        return self.canvas.create_polygon(points, **kwargs, smooth=True)
+        
+    def build(self) -> None:
+        self.link_input = PlaceholderEntry(self.root, width=30, placeholder="Cole seu link aqui",
+            borderwidth=0, highlightthickness=0, font=("JetBrainsMono", 12), insertbackground="#ccc",
+            background="#111", fg="#fff")
+        self.link_input.grid(row=0, column=0, padx=(15, 5), pady=10)
+        
+        self.video_button = tk.Button(self.root, image=self.video_img, bg="#111", borderwidth=0, highlightthickness=0, 
+                           command=lambda: self.validate_link(self.link_input.get(), False), activebackground="#111")
+        self.video_button.grid(row=0, column=1, padx=7, pady=20)
+
+        self.music_button = tk.Button(self.root, image=self.music_img, bg="#111", borderwidth=0, highlightthickness=0,
+                           command=lambda: self.validate_link(self.link_input.get(), True), activebackground="#111")
+        self.music_button.grid(row=0, column=2, padx=7, pady=20)
+        
+        close_button = tk.Button(self.root, image=self.close_img, bg="#111", borderwidth=0, highlightthickness=0, 
+                           command=lambda: self.root.destroy(), activebackground="#111")
+        close_button.grid(row=0, column=3, padx=7, pady=20)
+        
+    def start_move(self, event) -> None:
+        self.offset_x = event.x
+        self.offset_y = event.y
+
+    def move_window(self, event) -> None:
+        x = event.x_root - self.offset_x
+        y = event.y_root - self.offset_y
+        self.root.geometry(f"+{x}+{y}")
+      
     def get_screen_dimensions(self) -> Tuple[int, int]:
         user32 = windll.user32
         screen_width: int = user32.GetSystemMetrics(0)
         screen_height: int = user32.GetSystemMetrics(1)
         return screen_width, screen_height
 
-    def icon_(self, src: Path) -> ft.Image:
-        return ft.Image(src=src, width=20, height=20)
-
-    def main(self, page: ft.Page) -> None:
-        self.page = page
-        page.title = self.app_name
-        page.window.icon = self.icon_window
-        page.window.resizable = False
-        page.window.maximizable = False
-        page.window.title_bar_hidden = True
-        page.window.title_bar_buttons_hidden = True
-        page.bgcolor = "#111111"
-        page.window.on_close = self.cleanup
-        page.fonts = {"JetBrainsMono": "./src/fonts/JetBrainsMono-Regular.ttf"}
-        page.theme = ft.Theme(font_family="JetBrainsMono")
-
-        w, h = 400, 60
-        self.page.window.width, self.page.window.height = w, h
-        self.page.window.max_width, self.page.window.max_height = w, h
-        self.page.window.left = self.screen_width - (w + 15)
-        self.page.window.top = self.screen_height - (h + 65)
-        self.video_icon: ft.Image = self.icon_(self.default_video_icon)
-        self.music_icon: ft.Image = self.icon_(self.default_music_icon)
-        self.video_indicator: ft.Container = ft.Container()
-        self.music_indicator: ft.Container = ft.Container()
-
-        self.search_input: ft.TextField = ft.TextField(
-            hint_text="Cole seu link aqui",
-            width=266,
-            height=60,
-            text_size=14,
-            border=ft.InputBorder.NONE,
-            color=ft.colors.WHITE,
-            on_change=lambda e: (
-                setattr(self, 'link_download', e.control.value),
-                self.reset_hint_text()
-            ),
-            on_submit=lambda e: self.validate_link(self.link_download, False),
-        )
-
-        self.close_button: ft.Container = ft.Container(
-            content=ft.Icon(
-                name=ft.icons.CHEVRON_RIGHT,
-                color=ft.colors.GREY,
-                size=20,
-                opacity=.4,
-            ),
-            on_click=lambda e: self.cleanup(e),
-        )
-        self.page.update()
-
-        self.page.add(
-            ft.WindowDragArea(
-                ft.Container(
-                    content=ft.Row(
-                        [
-                            ft.Container(
-                                content=self.search_input,
-                                padding=ft.padding.only(left=5),
-                                margin=ft.margin.only(top=-8),
-                                height=60,
-                            ),
-                            ft.Container(
-                                content=ft.Row(
-                                    [
-                                        self.video_indicator,
-                                        ft.Container(
-                                            content=self.music_indicator,
-                                            margin=ft.margin.only(right=-4),
-                                        ),
-                                        self.close_button,
-                                    ]
-                                ),
-                                margin=ft.margin.only(left=5),
-                            )
-                        ]
-                    ),
-                ),
-                expand=True
-            ),
-        )
-
-        self.update_buttons()
-        self.page.update()
-
-    def reset_hint_text(self) -> None:
-        self.search_input.hint_text = "Cole seu link aqui"
-        self.page.update()
-
     def validate_link(self, link: str, audio: bool) -> None:
-        if not link or self.downloading:
+        if link == "Cole seu link aqui" or not link.strip() or self.downloading:
             return
 
         link = link.strip()
@@ -141,23 +153,6 @@ class SnapDL:
 
         self.download(link, audio)
 
-    def clear_temp_content(self) -> None:
-        temp_folder = path.join(getenv("TEMP"), "downloader_temp")
-        if path.exists(temp_folder):
-            for file in listdir(temp_folder):
-                try:
-                    remove(path.join(temp_folder, file))
-                except Exception as ex:
-                    print(f"Erro ao remover arquivo temporário: {ex}")
-            try:
-                rmdir(temp_folder)
-            except Exception as ex:
-                print(f"Erro ao remover diretório temporário: {ex}")
-
-    def cleanup(self, e) -> None:
-        self.clear_temp_content()
-        e.page.window.close()
-
     def get_music_folder(self) -> Path:
         return Path.home() / "Music"
 
@@ -167,6 +162,9 @@ class SnapDL:
     def download(self, link: str, audio: Optional[bool] = False) -> None:
         if not link:
             return
+        
+        self.video_button.config(state=tk.DISABLED)
+        self.music_button.config(state=tk.DISABLED)
     
         if any(service in link for service in ["deezer", "soundcloud", "youtube_music", "apple_music"]):
             audio = True
@@ -174,7 +172,6 @@ class SnapDL:
         temp_folder = path.join(getenv("TEMP"), "downloader_temp")
         makedirs(temp_folder, exist_ok=True)
         output_path_temp = path.join(temp_folder, f'%(title)s.%(ext)s')
-
         link_patterns: Dict[str, str] = {
             "reels do Instagram": r'https://www\.instagram\.com/reel/[A-Za-z0-9]+/?',
             "twitter": r'https://x\.com/[A-Za-z0-9_]+/status/[0-9]+',
@@ -211,7 +208,6 @@ class SnapDL:
 
         self.downloading = True
         self.downloading_type = not audio
-        self.update_buttons()
 
         try:
             with YoutubeDL(ydl_opts) as ydl:
@@ -235,7 +231,6 @@ class SnapDL:
                 count += 1
 
             rename(path.join(temp_folder, downloaded_file), final_path)
-
             self.icon_status(True, audio)
             self.toast_info("Download finalizado", "O arquivo foi baixado com sucesso.", final_path, audio)
 
@@ -249,59 +244,53 @@ class SnapDL:
             self.toast_info("Erro inesperado", "Ocorreu um erro inesperado.\nTente novamente em alguns instantes.")
             self.icon_status(False, audio)
         finally:
+            self.video_button.config(state=tk.NORMAL)
+            self.music_button.config(state=tk.NORMAL)
             self.clear_temp_content()
-
-        sleep(4)
-        self.reset()
-
-    def reset(self) -> None:
-        self.link_download = ""
-        self.search_input.value = ""
-        self.downloading = False
-        self.update_buttons()
-
+    
     def icon_status(self, conclude: bool, audio: bool) -> None:
+        success_icon = PhotoImage(file=r"./src/assets/check.png")
+        error_icon = PhotoImage(file=r"./src/assets/error.png")
+
         if audio:
-            self.music_icon.src = self.conclude_icon if conclude else self.fail_icon
-            self.music_indicator.content = self.music_icon
+            if conclude:
+                self.music_button.config(image=success_icon)
+                self.music_button.image = success_icon
+            else:
+                self.music_button.config(image=error_icon)
+                self.music_button.image = error_icon
         else:
-            self.video_icon.src = self.conclude_icon if conclude else self.fail_icon
-            self.video_indicator.content = self.video_icon
-        self.page.update()
+            if conclude:
+                self.video_button.config(image=success_icon)
+                self.video_button.image = success_icon
+            else:
+                self.video_button.config(image=error_icon)
+                self.video_button.image = error_icon
 
-    def update_buttons(self) -> None:
-        size: int = 20
-        loading: ft.ProgressRing = ft.ProgressRing(width=size, height=size)
-
-        if self.downloading:
-            indicator = self.video_indicator if self.downloading_type else self.music_indicator
-            indicator.content = loading
-        else:
-            self.video_indicator.content = self.icon_(self.default_video_icon)
-            self.video_indicator.on_click = lambda _: self.download(self.link_download, False)
-            self.music_indicator.content = self.icon_(self.default_music_icon)
-            self.music_indicator.on_click = lambda _: self.download(self.link_download, True)
-
-        self.page.update()
+        self.root.update()
 
     def toast_info(self, title: str, msg: str, path: Optional[str] = None, audio: Optional[bool] = None) -> None:
-        from winotify import Notification
-        toast = Notification(
-            app_id=self.app_name,
-            title=title,
-            msg=msg,
-            duration="short",
-            icon=self.icon,
-        )
-
+        toast = Notification( app_id="SnapDL", title=title, msg=msg, duration="short", icon=self.icon)
         if path:
             if audio is not None:
                 local_files: str = str(self.get_music_folder()) if audio else str(self.get_video_folder())
                 toast.add_actions(label="Abrir diretório", launch=f"file:///{local_files}")
-
             toast.add_actions(label="Abrir arquivo", launch=f"file:///{path}")
-
         toast.show()
+
+    def clear_temp_content(self) -> None:
+        temp_folder = path.join(getenv("TEMP"), "downloader_temp")
+        if path.exists(temp_folder):
+            for file in listdir(temp_folder):
+                try:
+                    remove(path.join(temp_folder, file))
+                except Exception as ex:
+                    print(f"Erro ao remover arquivo temporário: {ex}")
+            try:
+                rmdir(temp_folder)
+            except Exception as ex:
+                print(f"Erro ao remover diretório temporário: {ex}")
+
 
 if __name__ == "__main__":
     SnapDL()
